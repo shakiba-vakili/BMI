0ff6f84be5ee14a83ce23a3b903581ca47901f83
Shakiba Sadat Vakili <122327036+shakiba-vakili@users.noreply.github.com>
2023-05-28 20:07:20 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const form = document.querySelector("form");
+
+function calculateBmi(height, weight) {
+  height = height / 100;
+  let result = weight / (height * height);
+  return result.toFixed(2);
+}
+
+function clearResult() {
+  document.querySelector("#result").innerHTML = "";
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  clearResult();
+
+  let height = document.getElementById("height").value;
+  let weight = document.getElementById("weight").value;
+
+  if (height.trim() === "" || weight.trim() === "") {
+    alert("لطفا همه‌ی ورودی‌ها را پر کنید!");
+
+    return false;
+  }
+
+  height = parseInt(height);
+  weight = parseInt(weight);
+
+  if (isNaN(height) || isNaN(weight)) {
+    alert("قد و وزن باید عدد باشند.");
+
+    return false;
+  }
+
+  if (weight < 0 || height < 0) {
+    alert("قد و وزن نمی‌توانند کمتر از صفر باشند.");
+
+    return false;
+  }
+
+  document.querySelector("#result").innerHTML = calculateBmi(height, weight);
+});
+
+document.querySelector("input#height").addEventListener("keydown", clearResult);
+document.querySelector("input#weight").addEventListener("keydown", clearResult);
